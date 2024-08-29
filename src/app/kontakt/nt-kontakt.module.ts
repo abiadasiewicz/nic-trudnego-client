37b3c7c2391a66b5e0da54843d03157e33b6d61f
abiadasi <adam.biadasiewicz@capgemini.com>
2024-08-29 11:55:59 +0200

    Revert "some bad colors"
    This reverts commit ac8eeec62e80050daf9d36b0aa05c02dec96c384.

--- src/app/kontakt/nt-kontakt.module.ts
+++ src/app/kontakt/nt-kontakt.module.ts
@@ -1,23 +1,20 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import {NtKontaktRoutingModule} from './nt-kontakt-routing.module';
+import { NtKontaktRoutingModule } from './nt-kontakt-routing.module';
 import {NtEmailFormComponent} from './component/nt-email-form/nt-email-form.component';
-import {NtKontaktComponent} from './component/nt-kontakt/nt-kontakt.component';
+import { NtKontaktComponent } from './component/nt-kontakt/nt-kontakt.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
-    declarations: [NtEmailFormComponent, NtKontaktComponent],
-    imports: [
-        CommonModule,
-        NtKontaktRoutingModule,
-        ReactiveFormsModule,
-        MatInputModule,
-        MatButtonModule
-    ]
+  declarations: [NtEmailFormComponent, NtKontaktComponent],
+  imports: [
+    CommonModule,
+    NtKontaktRoutingModule,
+    ReactiveFormsModule,
+    MatInputModule
+  ]
 })
-export class NtKontaktModule {
-}
+export class NtKontaktModule { }
