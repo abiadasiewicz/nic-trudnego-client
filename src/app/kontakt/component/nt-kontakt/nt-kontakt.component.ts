a89296893b7414c27623d1b2bc363dd1e6e8454b
abiadasi <adam.biadasiewicz@capgemini.com>
2024-09-03 19:53:42 +0200

    Refactor contact

--- src/app/kontakt/component/nt-kontakt/nt-kontakt.component.ts
+++ src/app/kontakt/component/nt-kontakt/nt-kontakt.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'nt-nt-kontakt',
   templateUrl: './nt-kontakt.component.html',
   styleUrls: ['./nt-kontakt.component.scss']
 })
-export class NtKontaktComponent {
+export class NtKontaktComponent implements OnInit {
+  zoom = 12;
+  // @ts-ignore
+  center: google.maps.LatLngLiteral | google.maps.LatLng;
+  options: google.maps.MapOptions = {
+    mapTypeId: 'hybrid',
+    zoomControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    maxZoom: 15,
+    minZoom: 8,
+  };
 
+  ngOnInit() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.center = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+    });
+  }
+
+  // zoomIn() {
+  //   if (this.zoom < this.options?.maxZoom) this.zoom++;
+  // }
+  //
+  // zoomOut() {
+  //   if (this.zoom > this.options?.minZoom) this.zoom--;
+  // }
 }
--- src/app/kontakt/nt-kontakt.module.ts
+++ src/app/kontakt/nt-kontakt.module.ts
@@ -4,14 +4,16 @@ import { CommonModule } from '@angular/common';
 import { NtKontaktRoutingModule } from './nt-kontakt-routing.module';
 import { NtKontaktComponent } from './component/nt-kontakt/nt-kontakt.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import {GoogleMap} from '@angular/google-maps';
 
 
 @NgModule({
   declarations: [NtKontaktComponent],
-  imports: [
-    CommonModule,
-    NtKontaktRoutingModule,
-    ReactiveFormsModule
-  ]
+    imports: [
+        CommonModule,
+        NtKontaktRoutingModule,
+        ReactiveFormsModule,
+        GoogleMap
+    ]
 })
 export class NtKontaktModule { }
